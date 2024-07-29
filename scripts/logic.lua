3c37c781735b555adca6fda08ecc3a762ad18df8
Elia Robyn Lake <gh@arborelia.net>
2024-07-29 01:49:51 -0400

    logic fixes, especially in the dream world

--- scripts/logic.lua
+++ scripts/logic.lua
@@ -9,7 +9,7 @@ function has(item, amount)
 end
 
 function dream()
-    return has("raftpiece") or has("open-dream")
+    return has("raftpiece")
 end
 
 function dreamchange()
@@ -17,41 +17,127 @@ function dreamchange()
 end
 
 function wizardry()
-    return has("open-dream") or (has("raftpiece") and has("forcewand"))
+    return dream() and (has("open-dream") or has("forcewand"))
+end
+
+function wizardryCompletable()
+    return wizardry() and (
+        dreamchange() or 
+        (has("dreams-keep-items") and has("stick") and has("wand"))
+    )
+end
+
+function wizardryCompletableInLogic()
+    return wizardryCompletable()
 end
 
 function bottomless()
-    return has("open-dream") or (has("raftpiece") and has("icering"))
+    return dream() and (has("open-dream") or has("icering"))
+end
+
+function bottomlessCompletable()
+    if not bottomless() then
+        return false
+    elseif has("dreams-keep-items") then
+        if has("sword") and has("icering") then
+            return keys("dream2", 2)
+        elseif has("stick") and has("icering") then
+            return keys("dream2", 4)
+        else
+            return false
+        end
+    else
+        return keys("dream2", 4)
+    end
+end
+
+function bottomlessCompletableInLogic()
+    if not bottomless() then
+        return false
+    elseif has("dreams-keep-items") then
+        return has("icering") and has("roll") and has("stick") and realkeys("dream2", 4)
+    else
+        return realkeys("dream2", 4)
+    end
 end
 
 function syncope()
-    return has("open-dream") or (has("raftpiece") and has("dynamite"))
+    return dream() and (has("open-dream") or has("dynamite"))
+end
+
+function syncopeCompletable()
+    if not syncope() then
+        return false
+    elseif has("dreams-keep-items") then
+        return (
+            has("dynamite") and has("icering") and has("wand")
+        ) or (
+            has("dynamite")
+            and (has("icering") or has("wand"))
+            and keys("dream3", 1)
+        ) or (
+            has("dynamite") and nondynamite() and keys("dream3", 3)
+        )
+    else
+        return keys("dream3", 3)
+    end
+end
+
+function syncopeCompletableInLogic()
+    if not syncope() then
+        return false
+    elseif has("dreams-keep-items") then
+        return (
+            has("dynamite") and has("icering") and has("wand")
+        ) or (
+            has("dynamite") and nondynamite() and realkeys("dream3", 3)
+        )
+    else
+        return realkeys("dream3", 3)
+    end
 end
 
 function antigram()
-    return has("open-dream") or (has("raftpiece") and has("sword") and has("chain"))
+    return dream() and (has("open-dream") or (has("sword") and has("chain")))
+end
+
+function antigramCompletable()
+    if not antigram() then
+        return false
+    elseif has("dreams-keep-items") then
+        return weapon() and keys("dream4", 4)
+    else
+        return keys("dream4", 4)
+    end
+end
+
+function antigramCompletableInLogic()
+    if not antigram() then
+        return false
+    elseif has("dreams-keep-items") then
+        return weapon() and has("roll") and realkeys("dream4", 4)
+    else
+        return realkeys("dream4", 4)
+    end
 end
 
 function quietus()
-    if has("open-dream") then
-        return keys("dream2", 4) and keys("dream4", 4)
-    elseif has("raftpiece") and has("firemace") and has("forcewand") and has("sword") and has("icering") and has("chain") then
-        return keys("dream2", 2) and keys("dream4", 4)
-    else
-        return false
-    end
+    return (
+        wizardryCompletable()
+        and bottomlessCompletable()
+        and syncopeCompletable()
+        and antigramCompletable()
+    )
 end
 
 function quietus_in_logic()
-    if has("open-dream") then
-        return keys("dream2", 4) and keys("dream4", 4)
-    elseif has("raftpiece") and has("firemace") and has("forcewand") and has("sword") and has("icering") and has("chain") and has("roll") then
-        return realkeys("dream2", 4) and realkeys("dream4", 4)
-    else
-        return false
-    end
+    return (
+        wizardryCompletableInLogic()
+        and bottomlessCompletableInLogic()
+        and syncopeCompletableInLogic()
+        and antigramCompletableInLogic()
+    )
 end
-
 
 function weapon()
     return has("stick") or has("forcewand") or has("icering") or has("dynamite")
